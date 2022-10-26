9980419fcc98241299b33ee3d380641c40268e9c
Oleksandr Barabash <barabashsan4ik@gmail.com>
2022-10-26 18:12:25 +0300

    add feedback

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,35 +1,15 @@
-import PropTypes from "prop-types";
-import {
-  StatisticsSection,
-  Title,
-  ListStat,
-  Label,
-  Percentage,
-  Item
-} from "./Statistics.styled";
+import { StatisticsContainer, StatisticsValue } from "./Statistics.dtyled";
 
-export default function Statistics({ data }) {
+export default function Statistics({ good, neutral, bad, total, goodPercent }) {
   return (
-    <StatisticsSection>
-      <Title>Upload stats</Title>
-      <ListStat>
-        {data.map((item) => (
-          <Item id={item.id} key={item.id}>
-            <Label>{item.label}: </Label>
-            <Percentage>{item.percentage}%</Percentage>
-          </Item>
-        ))}
-      </ListStat>
-    </StatisticsSection>
+    <div>
+      <StatisticsContainer>
+        <StatisticsValue> Good: {good}</StatisticsValue>
+        <StatisticsValue> Neutral: {neutral}</StatisticsValue>
+        <StatisticsValue> Bad: {bad}</StatisticsValue>
+      </StatisticsContainer>
+      <span> Total: {total}</span>
+      <span> Positive feedback: {goodPercent}% </span>
+    </div>
   );
 }
-
-Statistics.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    })
-  ),
-};
